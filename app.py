651be8ac577c62120f8b124089cbb98d3f9aaa9b
Muhammad Irfan <irfan.ai.engineer@gmail.com>
2026-09-10 19:17:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ with open("MLR.pickle", "rb") as f:
 
 st.title("House Price Prediction")
 area_sqft = st.number_input("Area_sqft", min_value=0.0, max_value=20000.0, step=10.0)
-bedrooms = st.number_input("Bedrooms", min_value=0, max_value=10, step=1.0)
-bathrooms = st.number_input("Bathrooms", min_value=0, max_value=10, step=1.0)
+bedrooms = st.number_input("Bedrooms", min_value=0, max_value=10, step=1)
+bathrooms = st.number_input("Bathrooms", min_value=0, max_value=10, step=1)
 age_years = st.number_input("Age of House (year)", min_value=0.0, max_value=100.0, step=1.0)
 distance = st.number_input("Distance to city km", min_value=0.0, max_value=100.0, step=0.5)
 
